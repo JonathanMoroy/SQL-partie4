4d4c544338eb47707a5660d83da54a009abef1a7
JonathanMoroy <jonathan.moroy@gmail.com>
2017-06-03 14:19:21 +0200

    Fin des exercices de la partie 4

---
+++ index.sql
@@ -0,0 +1,15 @@
+USE `webDevelopment`;
+INSERT INTO `languages` (`language`, `version`)
+     VALUES   ('JavaScript', '5'),
+         ('PHP', '5.2'),
+         ('PHP', '5.4'),
+         ('HTML', '5.1'),
+         ('JavaScript', '6'),
+         ('JavaScript', '7'),
+         ('JavaScript', '8'),
+         ('PHP', '7');
+INSERT INTO `frameworks` (`framework`, `version`)
+     VALUES   ('Symfony', '2.8'),
+         ('Symfony', '3'),
+         ('Jquery', '1.6'),
+         ('Jquery', '2.10');
